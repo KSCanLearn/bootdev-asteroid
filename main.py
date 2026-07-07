ae9f8a9a8b3f2a6415caaa5db5dbf7633ccf5bd5
kscanlearn <kscanlearn@gmail.com>
2026-07-07 22:42:02 +0800

    Enhance Player class with rotation and update methods; refactor main loop for improved rendering

--- main.py
+++ main.py
@@ -17,9 +17,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            player.draw(screen)
-            screen.fill("black")
-            pygame.display.flip()
+        screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
+
+        pygame.display.flip()
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
