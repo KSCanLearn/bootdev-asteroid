288c23dce7acdd0887dc9081c22870863d2d0a86
kscanlearn <kscanlearn@gmail.com>
2026-07-20 22:17:17 +0800

    Implement asteroid splitting logic: update split method to create smaller asteroids upon collision

--- models/asteroid.py
+++ models/asteroid.py
@@ -1,5 +1,8 @@
+import random
+
+from logger import log_event
 from models.circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import ASTEROID_MIN_RADIUS, LINE_WIDTH
 import pygame
 
 class Asteroid(CircleShape):
@@ -11,4 +14,20 @@ class Asteroid(CircleShape):
     
     def update(self, dt: float) -> None:
         self.position += self.velocity * dt
-
+    
+    def split(self) -> None:
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+        rand = random.uniform(20, 50)
+        vec1 = self.velocity.rotate(rand)
+        vec2 = self.velocity.rotate(-rand)
+        
+        newRadius = self.radius - ASTEROID_MIN_RADIUS
+        
+        asteroid1 = Asteroid(self.position.x, self.position.y, newRadius)
+        asteroid2 = Asteroid(self.position.x, self.position.y, newRadius)
+        asteroid1.velocity = vec1 * 1.2
+        asteroid2.velocity = vec2 * 1.2
+        
